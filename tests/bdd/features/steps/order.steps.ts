be5fd7546e5671c2dbb7b82d869e71eab1f6533d
Luiz Júnior Lopes <55715995+luizjrlopes@users.noreply.github.com>
2025-08-19 19:11:54 -0300

    test: add scenarios for invalid orders

--- tests/bdd/features/steps/order.steps.ts
+++ tests/bdd/features/steps/order.steps.ts
@@ -5,6 +5,7 @@ import assert from 'assert';
 
 let connection: any;
 let lastOrderId: string;
+let lastResponse: any;
 
 Given('the order service is running', async function () {
   connection = await amqp.connect(process.env.RABBIT_URL || 'amqp://localhost');
@@ -16,6 +17,19 @@ Given('the order service is running', async function () {
 When('I create an order', async function () {
   const res = await axios.post('http://localhost:8080/orders', { productId: 'p1', quantity: 1 });
   lastOrderId = res.data.id;
+});
+
+When('I try to create an order with product {string} and quantity {int}', async function (productId: string, quantity: number) {
+  try {
+    const res = await axios.post('http://localhost:8080/orders', { productId, quantity });
+    lastResponse = res;
+  } catch (err: any) {
+    if (err.response) {
+      lastResponse = err.response;
+    } else {
+      throw err;
+    }
+  }
 });
 
 Then('an {string} event should be published', async function (routingKey: string) {
@@ -34,4 +48,9 @@ Then('an {string} event should be published', async function (routingKey: string
   assert.ok(msg, 'No message received');
   const content = JSON.parse(msg!.content.toString());
   assert.strictEqual(content.id, lastOrderId);
+});
+
+Then('the response status should be {int}', async function (status: number) {
+  assert.strictEqual(lastResponse?.status, status);
+  await connection.close();
 });
