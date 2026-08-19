b7f32f6d8458f80243e130ea5f3d663c3ba16089
Luiz Júnior M. S. Lopes <55715995+luizjrlopes@users.noreply.github.com>
2026-08-18 22:53:17 -0300

    feat: add API error mapping for missing orders

---
+++ services/api/order-service/src/main/java/com/shop/order/interfaces/ApiExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.shop.order.interfaces;
+
+import com.shop.order.domain.OrderNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ApiExceptionHandler {
+  @ExceptionHandler(OrderNotFoundException.class)
+  public ProblemDetail handleOrderNotFound(OrderNotFoundException exception) {
+    ProblemDetail problem = ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, exception.getMessage());
+    problem.setTitle("Order not found");
+    return problem;
+  }
+}
